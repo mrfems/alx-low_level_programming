7a96592b923efafe86100d5dc9bf1585274ecda7
mrfems <sliem.king@gmail.com>
2023-06-15 10:05:14 -0700

    First C script with betty

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,14 @@
+#include <stdio.>
+/**
+ * main - Point of entry
+ *
+ * Description: prints "Programming is like building a multilingual puzzle"
+ *
+ * Return: Prints 0 when successful
+ */
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
+
