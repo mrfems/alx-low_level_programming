fd553a8d2ca25f02c429fec157bd3bf906ad5484
mrfems <sliem.king@gmail.com>
2023-06-15 22:03:06 -0700

    Quotation mark

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,2 +1,16 @@
-#!/bin/bash
-gcc -c $CFILE
+#include <stdio.h>
+#include <unistd.h>
+
+/**
+ * main - print quotation mark
+ *
+ * Description: prints Dors's quote
+ *
+ * Return: Always 1 (Success)
+ */
+
+int main(void)
+{
+	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
+	return (1);
+}
