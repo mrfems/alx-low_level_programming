6e88292022a592cfe8d2c68636e082ad074c04bc
mrfems <sliem.king@gmail.com>
2023-06-18 00:50:05 -0700

    Print a-z using putchar

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ * main - main block
+ * Description: Generate and print alphabet in lowercase using putchar.
+ * Return: 0
+ */
+int main(void)
+{
+	char letta;
+
+	for(letta = 'a'; letta <= 'z'; letta++)
+	{
+		putchar(letta);
+	
+	}
+	putchar('\n');
+
+	return (0);
+}
