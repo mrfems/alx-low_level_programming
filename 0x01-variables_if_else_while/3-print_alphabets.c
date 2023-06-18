ebdc4256e21d245db07d8732e4a10fcd62f68195
mrfems <sliem.king@gmail.com>
2023-06-18 01:28:38 -0700

    Print a-z and A-Z using putchar

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+/**
+ * main - main block
+ * Description: Generate and print a-z/A-Z using putchar fx.
+ * Return: 0
+ */
+int main(void)
+{
+	char lower_case;
+	char upper_case;
+
+	for (lower_case = 'a'; lower_case <= 'z'; lower_case++)
+	{
+		putchar(lower_case);
+	}
+	putchar('\n');
+
+	for (upper_case = 'A'; upper_case <= 'Z'; upper_case++)
+        {
+                putchar(upper_case);
+        }
+        putchar('\n');
+
+	return (0);
+}
