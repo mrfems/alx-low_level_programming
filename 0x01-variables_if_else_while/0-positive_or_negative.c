150a19cda23c364a6258d765cb32f89a492fb505
mrfems <sliem.king@gmail.com>
2023-06-17 23:36:06 -0700

    Find negative, zero, or positive numbers

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -2,8 +2,8 @@
 #include <time.h>
 #include <stdio.h>
 /**
- * main - assigns a random number to int n everytime
- * it executes, and prints it
+ * main - assigns a random number to int n everytime the program runs
+ * it executes, and and check conditions, then  prints it
  * Return: Always 0 (Success)
  */
 int main(void)
@@ -12,11 +12,11 @@ int main(void)
 
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
-	if(n > 0)
+	if (n > 0)
 		printf("%d is positive\n", n);
-	else if(n == 0)
+	else if (n == 0)
 		printf("%d is zero\n", n);
-	else if(n < 0)
+	else if (n < 0)
 		printf("%d is negative\n", n);
 	return (0);
 }
