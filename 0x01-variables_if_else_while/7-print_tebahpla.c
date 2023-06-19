9420bd023ba56b6f8b589ad4b4bc6e43ab8886b1
mrfems <sliem.king@gmail.com>
2023-06-19 00:40:27 -0700

    Lowercase reversal fixed

--- 0x01-variables_if_else_while/7-print_tebahpla.c
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -9,7 +9,7 @@ int main(void)
 {
 	char lower_case;
 
-	for(lower_case = 'z'; lower_case >= 'a'; lower_case--)
+	for (lower_case = 'z'; lower_case >= 'a'; lower_case--)
 	{
 		putchar(lower_case);
 	}
