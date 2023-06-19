81e4b27c410208ea02aec9fb2133e846e80c0a63
mrfems <sliem.king@gmail.com>
2023-06-19 00:10:24 -0700

    gcc -Wall -pedantic -Werror -Wextra -std=gnu89 7-print_tebahpla.c -o 7-print_tebahpla

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - main block
+ * Description: Generate and print lowercase alphabet in reverse order.
+ * Return: 0
+ */
+int main(void)
+{
+ 	char lower_case;
+	
+	for(lower_case = 'z'; lower_case >= 'a'; lower_case--)
+	{
+		putchar(lower_case);
+	}
+	
+	putchar('\n');
+	return (0);	
+}
