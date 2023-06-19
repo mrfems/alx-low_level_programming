59966cc75fdca808275197b3d76e67ed74f6ca37
mrfems <sliem.king@gmail.com>
2023-06-18 23:56:41 -0700

    Base 10 numbers with putchar

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -9,11 +9,11 @@ int main(void)
 {
 	int base_10;
 
-	for (base_10 = 0; base_10 < 10; base_10++)
+	for (base_10 = 48; base_10 < 58; base_10++)
 	{
 		putchar(base_10);
 	}
 
-	printf("\n");
+	putchar('\n');
 	return (0);
 }
