5332560ed3dc82fd4f16cf6b9609d78f5427c4a1
mrfems <sliem.king@gmail.com>
2023-06-18 23:07:20 -0700

    Print all base 10 numbers, starting from zero

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - main block
+ * Description: Print all numbers of base 10, using for loop from 0.
+ * Return: 0
+ */
+int main(void)
+{
+	int base_10;
+	for (base_10 = 0; base_10 < 10; base_10++)
+	{
+		printf("%d", base_10);
+	}
+
+	printf("\n");
+	return (0);
+}
