259834d048d7bde6abc88a039bf5a7f2d45279fa
mrfems <sliem.king@gmail.com>
2023-06-19 07:39:47 -0700

    Comination of possible two tw0-digits number

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+
+/**
+ * main - main block
+ * Description: Generate and print all possible combinations of two two-digit numbers.
+ * Numbers should range from 0 to 99.
+  * Return: 0
+ */
+
+int main(void)
+{
+	int x, y;
+
+	for (x = 0; x < 100; x++)
+	{
+		for (y = 0; y < 100; y++)
+		{
+			if (x < y)
+			{
+				putchar((x / 10) + 48);
+				putchar((x % 10) + 48);
+				putchar(' ');
+				putchar((y / 10) + 48);
+				putchar((y % 10) + 48);
+				if (x != 98 || y != 99)
+				{
+					putchar(',');
+					putchar(' ');
+				}
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
+}
