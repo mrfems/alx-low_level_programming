887556cb8cbb63a16fb5ec73a885a2ad80e709b1
mrfems <sliem.king@gmail.com>
2023-06-19 01:18:02 -0700

    Comination of possible single digits

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+/**
+ * main - main block
+ * Description: Print all possible combinations fo single-digit numbers.
+ * The umbers must be separated by commas and a space.
+ * Return: 0
+ */
+
+int main(void)
+{
+	int combo;
+
+	for (combo = 48; combo < 58; combo++)
+	{
+		putchar(combo);
+		if (combo < 57)
+		{
+			putchar(44);
+			putchar(32);
+		}
+	}
+	putchar('\n');
+	return (0);
+}
