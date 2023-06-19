720b4a1cc591d34cc1d8858590612574aaa30e78
mrfems <sliem.king@gmail.com>
2023-06-19 00:14:34 -0700

    Generate lower z-a

--- 0x01-variables_if_else_while/7-print_tebahpla.c
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -7,13 +7,12 @@
  */
 int main(void)
 {
- 	char lower_case;
-	
+	char lower_case;
+
 	for(lower_case = 'z'; lower_case >= 'a'; lower_case--)
 	{
 		putchar(lower_case);
 	}
-	
 	putchar('\n');
 	return (0);	
 }
