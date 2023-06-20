eff1ae56180bccfc40238fddf61a16f7eabb2a05
mrfems <sliem.king@gmail.com>
2023-06-20 16:18:15 -0700

    Alphabet in lowercase using _putchar

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -2,7 +2,7 @@
 
 /**
  * Description: A program to general annd print a-z
- * print_alphabet - prints the alphabe, in lowercase, followed by a new line
+ * print_alphabet - prints the alphabets in lowercase, followed by a new line
  * Return: always success 0
  */
 void print_alphabet(void)
