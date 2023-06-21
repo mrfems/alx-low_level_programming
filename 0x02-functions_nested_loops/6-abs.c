c882237eb4b9d22367fc0d337ebdd2acfb07dbce
mrfems <sliem.king@gmail.com>
2023-06-20 17:03:44 -0700

    computes the absolute value of an integer

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ *_abs - Check Main code
+ * @r: An integre input
+ * Description: Returns absolute value of a number
+ * Return: Absolut value of number r
+ */
+int _abs(int r)
+{
+	if (r >= 0)
+		return (r);
+	else
+		return (r * -1);
+}
