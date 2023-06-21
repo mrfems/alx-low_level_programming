d389cf6b9efa9bf74f3768bdf39b9b4d682ab918
mrfems <sliem.king@gmail.com>
2023-06-21 12:28:08 -0700

    Fibonacci sequence

---
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -0,0 +1,23 @@
+include <stdio.h>
+/**
+ *main - print first 50 fibonacci
+ *
+ *Return: 0 always.
+ */
+int main(void)
+{
+	long int i, x = 1, y = 2, sum = 0, total_sum = 0;
+
+	for (i = 0; i < 49; i++)
+	{
+		if ((y % 2 == 0) && (y <= 4000000))
+		{
+			total_sum += y;
+		}
+		sum = x + y;
+		x = y;
+		y = sum;
+
+	}
+	printf("%ld\n", tSum);
+	return (0);
