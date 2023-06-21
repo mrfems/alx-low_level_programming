96805d5743251facca78b96cd4e0fa46a65931ac
mrfems <sliem.king@gmail.com>
2023-06-20 18:04:40 -0700

    unction that adds two integers and returns the result

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * add - Check Main
+ * @a: a integer a
+ * @b: b integer b
+ * Description: Adds two integers and returns the result
+ * Return: Result to add a and b
+ */
+int add(int a, int b)
+{
+	return (a + b);
+}
