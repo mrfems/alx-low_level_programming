9efce5a6b0ddd509dc8a9bf92f5ad82afc0e78ff
mrfems <sliem.king@gmail.com>
2023-06-22 15:50:51 -0700

    Remaining days of the year

--- 0x03-debugging/main.h
+++ 0x03-debugging/main.h
@@ -2,10 +2,9 @@
 #define MAIN_H
 
 #include <stdio.h>
-
-void positive_or_negative(int i);
 int largest_number(int a, int b, int c);
 int convert_day(int month, int day);
 void print_remaining_days(int month, int day, int year);
+void positive_or_negative(int i);
 
 #endif /* MAIN_H */
